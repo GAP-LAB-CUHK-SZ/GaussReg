e16cfa16940356814d240560e1a9c8df6b519313
Jiahao620 <1023499614@qq.com>
2025-07-01 02:43:31 +0800

    update script of rendering depth

--- wheels/gaussian_splatting/render.py
+++ wheels/gaussian_splatting/render.py
@@ -24,7 +24,6 @@ import numpy as np
 
 def render_set(model_path, name, iteration, views, gaussians, pipeline, background):
     render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "render_depths")
-    # gts_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt")
 
     makedirs(render_path, exist_ok=True)
     # makedirs(gts_path, exist_ok=True)
@@ -32,55 +31,9 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
 
     for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
         rendering = render(view, gaussians, pipeline, background)
-        """
-        height = 480
-        width = 640
-        y, x = torch.meshgrid([torch.arange(0, height, dtype=torch.float32, device=rendering["render"].device),
-                            torch.arange(0, width, dtype=torch.float32, device=rendering["render"].device)])
-        y, x = y.contiguous(), x.contiguous()
-        y, x = y.view(width * height), x.view(width * height)
-        xyz = torch.stack((x, y, torch.ones_like(x)))
-        xyz = torch.unsqueeze(xyz, 0).repeat(1, 1, 1)
-        from utils.graphics_utils import getWorld2View2, focal2fov, fov2focal
-        K = torch.zeros(3,3)
-        K[0,0] = fov2focal(view.FoVx, width)
-        K[1,1] = fov2focal(view.FoVy, height)
-        K[0,2] = width / 2
-        K[1,2] = height / 2
-        K[2,2] = 1.
-        xyz_c = torch.matmul(torch.linalg.inv(K[None,:,:]).to(xyz.device), xyz)
-        import cv2
-        depth = rendering["render_depth"][0].view(1,1,-1)
-        # depth  = torch.from_numpy(np.asarray(cv2.imread("./590.png", cv2.IMREAD_UNCHANGED)) / 1000).to(xyz.device).view(1,1,-1)
-        xyz_c = xyz_c * depth - torch.from_numpy(view.T).to(xyz.device).view(1,-1,1)
-        xyz_w = torch.matmul(torch.from_numpy(view.R).to(xyz.device).view(1,3,3), xyz_c)[0]
-        from plyfile import PlyData,PlyElement
-        points = xyz_w.cpu().numpy().transpose(1,0)
-        colors = rendering["render"].view(3, -1).cpu().numpy().transpose(1,0) * 255
-        alpha = rendering["render_alpha"].view(-1).cpu().numpy()
-        index = alpha > 0.9
-        points = points[index]
-        colors = colors[index]
-        # colors = np.asarray(cv2.pyrDown(cv2.imread("/workspace/data/dataset/train/scene0110_00/A/images/350.jpg"))).reshape(-1,3)
-        points = [(points[i,0], points[i,1], points[i,2], colors[i,0], colors[i,1], colors[i,2]) for i in range(points.shape[0])]
-        vertex = np.array(points, dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4'), ('red', 'uint8'), ('green', 'uint8'), ('blue', 'uint8')])
-        el = PlyElement.describe(vertex, 'vertex', comments=['vertices'])
-        PlyData([el], text=False).write("./scene_110_00_350_render.ply")
-        # if idx == 0:
-        #     depths = np.zeros((len(views), rendering.shape[0], rendering.shape[1]))
-        # depths[idx] = rendering.cpu().numpy()
-        """
         if idx == 0:
             depths = np.zeros((len(views), rendering["render_depth"][0].shape[0], rendering["render_depth"][0].shape[1]))
         depths[idx] = rendering["render_depth"][0].cpu().numpy()
-        # np.save(os.path.join(render_path, view.image_name + ".npy"), rendering["render_depth"][0].cpu().numpy())
-        # near = np.percentile(rendering["render_depth"][0].cpu().numpy(), 5)
-        # far = np.percentile(rendering["render_depth"][0].cpu().numpy(), 95)
-        # with open(os.path.join(render_path, view.image_name + ".txt"), 'w') as file:
-        #     file.write(str(near) + ' ' + str(far))
-        # gt = view.original_image[0:3, :, :]
-        # torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
-        # torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
         if idx == len(views) - 1:
             np.save(os.path.join(render_path, "depths.npy"), depths)
 
--- wheels/gaussian_splatting/render_depth.py
+++ wheels/gaussian_splatting/render_depth.py
@@ -2,13 +2,13 @@ import os
 import sys
 from tqdm import tqdm
 
-# dataset_path = "/workspace/data/dataset/test"
-dataset_path = "/workspace/data/geotransformer/data/outdoor"
+dataset_path = "ScanNet-GSReg/test"
+# dataset_path = "data/outdoor"
 scene_list = os.listdir(dataset_path)
 for scene_name in tqdm(scene_list):
-    scene_A_path = os.path.join(dataset_path, scene_name, "part1")
-    scene_B_path = os.path.join(dataset_path, scene_name, "part2")
-    A_command = 'CUDA_VISIBLE_DEVICES=1 python render.py --skip_test -m '+ str(scene_A_path)
-    B_command = 'CUDA_VISIBLE_DEVICES=1 python render.py --skip_test -m '+ str(scene_B_path)
+    scene_A_path = os.path.join(dataset_path, scene_name, "A")
+    scene_B_path = os.path.join(dataset_path, scene_name, "B")
+    A_command = 'CUDA_VISIBLE_DEVICES=1 python render.py --skip_test -m {model_A_path} --source_path {source_A_path}'.format(model_A_path=os.path.join(str(scene_A_path), 'output'), source_A_path=str(scene_A_path))
+    B_command = 'CUDA_VISIBLE_DEVICES=1 python render.py --skip_test -m {model_B_path} --source_path {source_B_path}'.format(model_B_path=os.path.join(str(scene_B_path), 'output'), source_B_path=str(scene_B_path))
     os.system(A_command)
     os.system(B_command)
